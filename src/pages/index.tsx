06c339e30fb88d2d10520314dd9113cec8f4c55a
Gui-dev <adrianon10@gmail.com>
2022-11-02 03:46:47 -0300

    feat: add src folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/code.png" />
+        <title>World Cup Sweepstakes</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
